889027a8745dcaa5a65a583c66f4751bbf0aece3
Anthony Wertz <awertz@pm.me>
2024-06-20 15:28:22 -0400

    Website refresh

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,43 @@
+var store = [{
+        "title": "Real-time Bayesian Aggregation",
+        "excerpt":"Nuclear threat detection is a complicated and difficult task. There are numerous challenges to be overcome in the process: radioactive material sourced for nefarious purposes will likely be shielded to prevent detection, yielding a very weakly detectable signal easily drowned out by background radiation from anything from buildings, concrete, to...","categories": [],
+        "tags": [],
+        "url": "/projects/2015-09-rtba/",
+        "teaser": "/assets/projects/prj_rtba.png"
+      },{
+        "title": "Microcirculatory Bloodflow Analysis",
+        "excerpt":"Advances in signal processing and miniaturization of sensors and electronics have allowed biomedical technology to improve by leaps and bounds in the last decade. Processing imaging sensor data in particular has made strides in helping to analyze and understand complicated and noisy sensor data in radiology, tomography, among others. In...","categories": [],
+        "tags": [],
+        "url": "/projects/2017-01-mcba/",
+        "teaser": "/assets/projects/prj_mcba.gif"
+      },{
+        "title": "Fast Detection of Hemorrhagic Shock",
+        "excerpt":"Biomedical data at the bedside and in the field is abundant, but underutilized. This is particularly true for fast detection of disease onset. In the Auton Lab I investigated the use of bedside monitoring data for fast detection of hemorrhagic shock. Figure 1: ROCORs showing detection performance based on featurization....","categories": [],
+        "tags": [],
+        "url": "/projects/2018-01-heme/",
+        "teaser": "/assets/projects/prj_heme.png"
+      },{
+        "title": "Hemodynamic Shock State Clustering",
+        "excerpt":"While we often strive to build intuitive and interpretable models based on data features we deem meaningful and potentially related to the phenomenon we aim to understand, this approach inherently limits the potential for the learned models to make novel discoveries and uncover previously unknown relationships. I had the privilege...","categories": [],
+        "tags": [],
+        "url": "/projects/2018-01-riss-nn/",
+        "teaser": "/assets/projects/prj_state_clustering.png"
+      },{
+        "title": "Counter Human Trafficking",
+        "excerpt":"Human trafficking is a disturbingly widespread problem in the United States, a reality I was largely unaware of prior to joining the Auton Lab. The lab does excellent work in understanding and combating human trafficking, even spinning off a company devoted to it. As part of CMU’s Robotics Institute Summer...","categories": [],
+        "tags": [],
+        "url": "/projects/2018-08-riss-trafficking/",
+        "teaser": "/assets/projects/prj_trafficking.png"
+      },{
+        "title": "Prediction of Postoperative Renal Failure after CABG Surgery",
+        "excerpt":"Biomedical data, while abundant, remains underutilized, even today. During my time at the Auton Lab, we often explored the potential of using such data to predict adverse medical outcomes in order to give advance warning and help clinicians provide timely care. I had the great privilege of mentoring Willa Potosnak...","categories": [],
+        "tags": [],
+        "url": "/projects/2019-08-riss-cabg/",
+        "teaser": "/assets/projects/prj_cabg.png"
+      },{
+        "title": "Data and Anomaly Visualization",
+        "excerpt":"Data, especially when there are large amounts of it, can quickly become unruly and difficult to understand. To this end, I have developed and collaborated on numerous data processing tools to make this task somewhat more tenable. This work spans my roles as analyst in the Auton Lab, doctoral student...","categories": [],
+        "tags": [],
+        "url": "/projects/2020-01-datavis/",
+        "teaser": "/assets/projects/ss_treeview_mouse.png"
+      }]
