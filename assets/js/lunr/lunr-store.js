acdba371a252570f8ad5867c3ae2526e25d8a349
Anthony Wertz <awertz@pm.me>
2024-06-26 17:31:31 -0400

    Updated with TRACIR and horton projects.

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,43 +1,55 @@
 var store = [{
-        "title": "Real-time Bayesian Aggregation",
+        "title": "Real-time Bayesian Aggregation (RTBA)",
         "excerpt":"Nuclear threat detection is a complicated and difficult task. There are numerous challenges to be overcome in the process: radioactive material sourced for nefarious purposes will likely be shielded to prevent detection, yielding a very weakly detectable signal easily drowned out by background radiation from anything from buildings, concrete, to...","categories": [],
         "tags": [],
         "url": "/projects/2015-09-rtba/",
         "teaser": "/assets/projects/prj_rtba.png"
       },{
-        "title": "Microcirculatory Bloodflow Analysis",
+        "title": "Microcirculatory Bloodflow Analysis (MCBA)",
         "excerpt":"Advances in signal processing and miniaturization of sensors and electronics have allowed biomedical technology to improve by leaps and bounds in the last decade. Processing imaging sensor data in particular has made strides in helping to analyze and understand complicated and noisy sensor data in radiology, tomography, among others. In...","categories": [],
         "tags": [],
         "url": "/projects/2017-01-mcba/",
         "teaser": "/assets/projects/prj_mcba.gif"
       },{
-        "title": "Fast Detection of Hemorrhagic Shock",
+        "title": "Fast detection of hemorrhagic shock",
         "excerpt":"Biomedical data at the bedside and in the field is abundant, but underutilized. This is particularly true for fast detection of disease onset. In the Auton Lab I investigated the use of bedside monitoring data for fast detection of hemorrhagic shock. Figure 1: ROCORs showing detection performance based on featurization....","categories": [],
         "tags": [],
         "url": "/projects/2018-01-heme/",
         "teaser": "/assets/projects/prj_heme.png"
       },{
-        "title": "Hemodynamic Shock State Clustering",
+        "title": "Physiologic state clustering in shock",
         "excerpt":"While we often strive to build intuitive and interpretable models based on data features we deem meaningful and potentially related to the phenomenon we aim to understand, this approach inherently limits the potential for the learned models to make novel discoveries and uncover previously unknown relationships. I had the privilege...","categories": [],
         "tags": [],
         "url": "/projects/2018-01-riss-nn/",
         "teaser": "/assets/projects/prj_state_clustering.png"
       },{
-        "title": "Counter Human Trafficking",
+        "title": "Counter human trafficking",
         "excerpt":"Human trafficking is a disturbingly widespread problem in the United States, a reality I was largely unaware of prior to joining the Auton Lab. The lab does excellent work in understanding and combating human trafficking, even spinning off a company devoted to it. As part of CMU’s Robotics Institute Summer...","categories": [],
         "tags": [],
         "url": "/projects/2018-08-riss-trafficking/",
         "teaser": "/assets/projects/prj_trafficking.png"
       },{
-        "title": "Prediction of Postoperative Renal Failure after CABG Surgery",
+        "title": "Postoperative renal failure prediction",
         "excerpt":"Biomedical data, while abundant, remains underutilized, even today. During my time at the Auton Lab, we often explored the potential of using such data to predict adverse medical outcomes in order to give advance warning and help clinicians provide timely care. I had the great privilege of mentoring Willa Potosnak...","categories": [],
         "tags": [],
         "url": "/projects/2019-08-riss-cabg/",
         "teaser": "/assets/projects/prj_cabg.png"
       },{
-        "title": "Data and Anomaly Visualization",
+        "title": "Data and anomaly visualization",
         "excerpt":"Data, especially when there are large amounts of it, can quickly become unruly and difficult to understand. To this end, I have developed and collaborated on numerous data processing tools to make this task somewhat more tenable. This work spans my roles as analyst in the Auton Lab, doctoral student...","categories": [],
         "tags": [],
         "url": "/projects/2020-01-datavis/",
         "teaser": "/assets/projects/ss_treeview_mouse.png"
+      },{
+        "title": "Trauma Care in a Rucksack (TRACIR)",
+        "excerpt":"Reasearch in robotics is profoundly changing what can be accomplished in medicine. This is especially true in the field, where patients may have limited or no direct access to the state-of-the art facilities and highly trained clinical staff you would find in a hospital or clinic. The Trauma Care in...","categories": [],
+        "tags": [],
+        "url": "/projects/2020-01-tracir/",
+        "teaser": "/assets/projects/prj_tracir.png"
+      },{
+        "title": "Soft manipulator control",
+        "excerpt":"Soft robotics in an exciting field of research focused on the design and application of robotic devices that rely heavily on soft materials for their novel functionality. This field presents several challenges: Complicated dynamics: Soft devices exhibit significant non-linear dynamics, hysteretic behavior, and time-dependent material property changes, making them more...","categories": [],
+        "tags": [],
+        "url": "/projects/2022-10-horton/",
+        "teaser": "/assets/projects/prj_trajopt.gif"
       }]
